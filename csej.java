5323c72a77b98e8916f7ff1731549bc58c8fabc6
sunxiao059 <165646153+sunxiao059@users.noreply.github.com>
2024-04-13 16:54:59 +0800

    Create csej.java

---
+++ csej.java
@@ -0,0 +1,9 @@
+public class Test { public static void main ( String [ ] args ) { int [ ] numbers = { 10,20,30,40,50 } ;​​​​​​​​
+ 
+       for ( int x :数字) {系统.出去。打印（x ）；
+         系统。出去。打印（” ，” ）;
+       }系统。出去。打印（“ \n ” ）；
+       String [ ] names = { “ James ” , “ Larry ” , “ Tom ” , “ Lacy ” } ;
+       for (字符串名称:名称) {系统.出去。打印（姓名）；
+         系统。出去。打印（” ，” ）;
+       } } }   
